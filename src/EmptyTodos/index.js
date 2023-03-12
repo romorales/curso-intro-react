778dc3ca7914cc2a3d4064eb277e97b0f30c421b
Rocio Morales <roma.morales12@gmail.com>
2023-03-11 22:52:04 -0300

    EmptyTodos

--- src/EmptyTodos/index.js
+++ src/EmptyTodos/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import './EmptyTodos.css'
+import addTaskIcon from '../Images/addTask.png'
+import { TodoContext } from "../TodoContext";
+
 
 function EmptyTodos() {
- return <p>Crea tu primer TODO!</p>;
+const { openModal, setOpenModal } = React.useContext(TodoContext);
+
+const onClickButton = ()=>{
+    //props.setOpenModal(prevState => !prevState)
+    setOpenModal(!openModal);
+}
+
+ return <div className='EmptyTodos'>
+    <h2>¡UPS! Lista de tareas vacía</h2>
+    <img alt="add" width='16%' src={addTaskIcon} onClick={ onClickButton }></img>
+    <p>Presiona ➕ para agregar una nueva</p>
+
+    </div>;
 }
 
 export { EmptyTodos };
--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -13,7 +13,7 @@ import './TodoSearch.css';
       <div className="TodoSearch"> 
           <input 
             className="TodoSearch-input" 
-            placeholder='Ingresar tarea'
+            placeholder='Filtrar por nombre de tarea'
             value={searchValue}
             onChange={onSearchValueChange}/>
       </div> 
